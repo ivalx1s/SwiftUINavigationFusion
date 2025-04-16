a4364cb07e76f0d4367d784f5c54a0c8d1c3758e
Ivan Oparin <oparin@me.com>
2025-04-17 00:44:20 +0400

    Initial Commit

---
+++ NavigationFusion/NavigationFusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationFusionApp.swift
+//  NavigationFusion
+//
+//  Created by Ivan Wb on 17.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct NavigationFusionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
